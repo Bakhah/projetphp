5b7dbfb31938604bcfdc5d2e485dd4a66ef946cc
bakhah <franck.hourdin@pictotravel.fr>
2016-09-14 18:11:59 +0200

    ClearDB setup

--- web/index.php
+++ web/index.php
@@ -7,6 +7,17 @@
 <?php
 echo "Projet de Louis Lalleau et Franck Hourdin";
 ?>
+<?php
+$url = parse_url(getenv("mysql://b1e7da43505e81:c262d860@us-cdbr-iron-east-04.cleardb.net/heroku_ac34ed3693a91e3?reconnect=true
+"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
 
 </body>
 </html>
