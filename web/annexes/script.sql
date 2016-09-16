c0a5ec9151ce183996f9c0d0b0c51ed80c8212f2
Louis <louis.lalleau@gmail.com>
2016-09-16 22:44:47 +0200

    Mon premier push sur gitKraken !

---
+++ web/annexes/script.sql
@@ -0,0 +1,62 @@
+/*CREATION DES TABLES*/
+CREATE DATABASE PHP_PROJECT;
+USE PHP_PROJECT;
+CREATE TABLE product(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  price FLOAT NOT NULL,
+  id_unit INT NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE unit(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE recipe(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  instruction TEXT,
+  id_user INT NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE needs(
+  id INT NOT NULL AUTO_INCREMENT,
+  id_product INT NOT NULL,
+  quantity FLOAT NOT NULL,
+  id_recipe INT NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE user(
+  id INT NOT NULL AUTO_INCREMENT,
+  login VARCHAR(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  admin BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
+);
+CREATE TABLE menu(
+  id INT NOT NULL AUTO_INCREMENT,
+  jour DATE NOT NULL,
+  id_recipe INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+/*ENRICHISSEMENT DES TABLES*/
+USE PHP_PROJECT;
+INSERT INTO user(login,password,admin) VALUES
+('admin','admin',1),
+('user','user',0);
+INSERT INTO unit(name) VALUES
+('litre'),
+('kigo'),
+('pièces');
+INSERT INTO product(name,price,id_unit) VALUES
+('lait de soja',2.50,1),
+('Oeuf',0.20,3),
+('riz',0.80,2);
+INSERT INTO recipe(name,instruction,id_user) VALUES
+('Omelette',"<p>1. Casser les oeufs</p><p>Battre les oeufs</p><p>cuire les oeufs</p>",2);
+INSERT INTO needs(id_product,quantity,id_recipe) VALUES
+(2,200,1);
+INSERT INTO menu(jour,id_recipe) VALUES
+('23-06-2016',1);
--- web/index.php
+++ web/index.php
@@ -2,22 +2,22 @@
 <html>
 <body>
 
-<h1>PROJET</h1>
+  <h1>PROJET</h1>
 
-<?php
-echo "Projet de Louis Lalleau et Franck Hourdin";
-?>
-<?php
-$url = parse_url(getenv("mysql://b1e7da43505e81:c262d860@us-cdbr-iron-east-04.cleardb.net/heroku_ac34ed3693a91e3?reconnect=true
-"));
+  <?php
+  echo "Projet de Louis Lalleau et Franck Hourdin";
+  ?>
+  <?php
+  $url = parse_url(getenv("mysql://b1e7da43505e81:c262d860@us-cdbr-iron-east-04.cleardb.net/heroku_ac34ed3693a91e3?reconnect=true
+  "));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"], 1);
 
-$conn = new mysqli($server, $username, $password, $db);
-?>
+  $conn = new mysqli($server, $username, $password, $db);
+  ?>
 
 </body>
 </html>
